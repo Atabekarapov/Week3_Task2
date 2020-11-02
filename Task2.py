51aa0884ca85c040c5231371bdcfe634a51f4a64
Atabek <atabekarapov14@gmail.com>
2020-11-02 13:35:28 +0600

    Week3_Task2

---
+++ Task2.py
@@ -0,0 +1,62 @@
+# Week3.Task2 -----> Atabek (HOMEWORK)
+# 1)
+# best = input('Enter the number wihout space: ')
+# list_ = []
+# for i in best:
+#     list_.append(i)
+# print('Largest element is: ', max(list_))
+
+# 2)  
+# import random
+# while True :
+#     number = random.randint(1,50)
+
+#     while True:
+#         print('What do you think what number?') 
+#         guess = input()
+#         guess = int(guess)
+#         if guess < number:
+#             print('Ugadaite bolshe') 
+#         if guess > number:
+#             print('Ugadaite menshe!!')
+#         if guess == number:
+#             break
+#     if guess == number:
+#         print('WWWOWWW ' +  '! You got the number !!')
+#         continue_ = input('Chtoby vyiti, najmite (exit) ')
+#         if continue_ == 'exit' or 'Exit':
+#             break      
+#         else:
+#             print('I said Exit!!')
+
+# 3)
+# n = int(input('Any number: '))
+# factorial = 1
+# while n > 1:
+#     factorial *= n
+#     n -= 1
+
+# print(factorial)
+
+# 4)
+# n = int(input("Type a number: "))
+# count = 10
+# while count >= 1:
+#     print(f"{count} x {n} = {n * count}")
+#     count = count - 1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
